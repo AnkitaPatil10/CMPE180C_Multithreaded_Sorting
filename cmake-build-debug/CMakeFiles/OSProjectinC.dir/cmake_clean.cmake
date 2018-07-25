e27fceb1aa8570606d721f137f6a198e914c1c24
Ankita Patil <ankita10patil@gmail.com>
2018-07-25 08:51:30 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OSProjectinC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSProjectinC.dir/main.c.o"
+  "OSProjectinC.pdb"
+  "OSProjectinC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSProjectinC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
